11eba3c74167f6dc3247d9878c5a0b07d36efb26
praveen_sanpada <praveen.sanpada@aurigait.com>
2022-06-03 15:13:33 +0530

    first

---
+++ custom/custom_message.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+
+    BODY_MISSING_PARAMS: "body missing parameters",
+
+    DB_CONN_ERROR: "databse connection error",
+
+    EMAIL_NOT_FOUND: "email not found",
+    PHONE_NOT_FOUND: "phone not found",
+    EMAIL_PHONE_NOT_FOUND: "email/phone not found",
+    EMAIL_PHONE_FOUND: "email/phone found",
+
+    EMAIL_VALIDATION: "please enter correct email id",
+    PHONE_VALIDATION: "please enter correct phone number",
+
+    USER_CREATED: "user created successfully",
+    USER_LOGIN: "user login successfully",
+    USER_LOGOUT: "user logout successfully",
+    USER_FETCHED: "users fetched successfully",
+    WRONG_PASSWORD: "wrong password",
+    USER_NOT_EXIT: "users not exits",
+
+    OTP_SENT: "otp sent successfully",
+    VERIFICATION_FAILED: "verification failed",
+    VERIFICATION_SUCCESS: "verification successfully"
+
+}
